f736b3004066872a8b90e9687f1353cca53a8f21
Bogdan Tanov <86558426+BogdanTanov@users.noreply.github.com>
2021-10-17 23:42:41 +0300

    Update pr4.py

--- pr4.py
+++ pr4.py
@@ -7,7 +7,7 @@ window.resize(650, 400)
 btn_OK = QPushButton("Ответить")
 lb_q = QLabel("Сколько будет 1000-7")
 
-RadioGroupBox = QGroupBox("Варианты   ответов")
+RadioGroupBox = QGroupBox("Варианты ответов")
 rbtn_1 = QRadioButton('69')
 rbtn_2 = QRadioButton('-1007')
 rbtn_3 = QRadioButton('-993')
@@ -43,6 +43,17 @@ layout_card.addLayout(layout_line2, stretch=8)
 layout_card.addStretch(1)
 layout_card.addLayout(layout_line3, stretch=1)
 layout_card.addStretch(1)
+
+def ok():
+	rbtn_1.hide()
+	rbtn_2.hide()
+	rbtn_3.hide()
+	rbtn_4.hide()
+	RadioGroupBox.hide()
+	ResultGroupBox = QGroupBox("Результат ответов")
+	layout_line2.addWidget(ResultGroupBox)
+
+btn_OK.clicked.connect(ok)
 
 window.setLayout(layout_card)
 window.show()
