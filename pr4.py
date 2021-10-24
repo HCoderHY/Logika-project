925d12f24e45018b89985a17261b76ef580c1014
Bogdan Tanov <86558426+BogdanTanov@users.noreply.github.com>
2021-10-24 19:23:09 +0300

    Update pr4.py

--- pr4.py
+++ pr4.py
@@ -1,5 +1,5 @@
 from PyQt5.QtCore import Qt
-from PyQt5.QtWidgets import QApplication, QWidget, QHBoxLayout, QVBoxLayout, QGroupBox, QRadioButton, QPushButton, QLabel
+from PyQt5.QtWidgets import QApplication, QWidget, QHBoxLayout, QVBoxLayout, QGroupBox, QRadioButton, QPushButton, QLabel, QButtonGroup
 
 app = QApplication([])
 window = QWidget()
@@ -12,6 +12,12 @@ rbtn_1 = QRadioButton('69')
 rbtn_2 = QRadioButton('-1007')
 rbtn_3 = QRadioButton('-993')
 rbtn_4 = QRadioButton('993')
+
+ButtonGroup = QButtonGroup()
+ButtonGroup.addButton(rbtn_1)
+ButtonGroup.addButton(rbtn_2)
+ButtonGroup.addButton(rbtn_3)
+ButtonGroup.addButton(rbtn_4)
 
 layout_ans1 = QHBoxLayout()
 layout_ans2 = QVBoxLayout()
@@ -27,12 +33,22 @@ layout_ans1.addLayout(layout_ans3)
 
 RadioGroupBox.setLayout(layout_ans1)
 
+ResultGroupBox = QGroupBox("Результат ответов:")
+layout_line2 = QVBoxLayout()
+ResultGroupBox.setLayout(layout_line2)
+tf = QLabel("Правильно/Неправильно")
+res = QLabel("Ответ:")
+layout_line2.addWidget(tf, alignment=(Qt.AlignLeft | Qt.AlignTop))
+layout_line2.addWidget(tf, alignment=(Qt.AlignCenter), stretch=2)
+ResultGroupBox.setLayout(layout_line2)
 layout_line1 = QHBoxLayout()
 layout_line2 = QHBoxLayout()
 layout_line3 = QHBoxLayout()
 
 layout_line1.addWidget(lb_q, alignment=(Qt.AlignCenter))
 layout_line2.addWidget(RadioGroupBox)
+layout_line2.addWidget(ResultGroupBox)
+ResultGroupBox.hide()
 layout_line3.addStretch(1)
 layout_line3.addWidget(btn_OK, stretch=3)
 layout_line3.addStretch(1)
@@ -45,15 +61,28 @@ layout_card.addLayout(layout_line3, stretch=1)
 layout_card.addStretch(1)
 
 def ok():
-	rbtn_1.hide()
-	rbtn_2.hide()
-	rbtn_3.hide()
-	rbtn_4.hide()
-	RadioGroupBox.hide()
-	ResultGroupBox = QGroupBox("Результат ответов")
-	layout_line2.addWidget(ResultGroupBox)
+    RadioGroupBox.hide()
+    ResultGroupBox.show()
+    btn_OK.setText("Следущий вопрос")
 
-btn_OK.clicked.connect(ok)
+def show_question():
+    RadioGroupBox.show()
+    ResultGroupBox.hide()
+    btn_OK.setText("Ответить")
+    ButtonGroup.setExclusive(False)
+    rbtn_1.setChecked(False)
+    rbtn_2.setChecked(False)
+    rbtn_3.setChecked(False)
+    rbtn_4.setChecked(False)
+    ButtonGroup.setExclusive(True)
+
+def test():
+    if 'Ответить' == btn_OK.text():
+        ok()
+    else:
+        show_question()
+
+btn_OK.clicked.connect(test)
 
 window.setLayout(layout_card)
 window.show()
