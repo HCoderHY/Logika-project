5ec356cf6300d6b845d1a0126ff8239046973188
Bogdan Tanov <86558426+BogdanTanov@users.noreply.github.com>
2021-10-31 19:03:34 +0200

    Update pr4.py

--- pr4.py
+++ pr4.py
@@ -42,6 +42,14 @@ res = QLabel()
 layout_line2.addWidget(tf, alignment=(Qt.AlignLeft | Qt.AlignTop))
 layout_line2.addWidget(res, alignment=(Qt.AlignCenter), stretch=2)
 ResultGroupBox.setLayout(layout_line2)
+StatisticsGroupBox = QGroupBox("Статистика опроса:")
+layout_line4 = QVBoxLayout()
+StatisticsGroupBox.setLayout(layout_line4)
+tf2 = QLabel("Результат")
+res2 = QLabel()
+layout_line4.addWidget(tf2, alignment=(Qt.AlignLeft | Qt.AlignTop))
+layout_line4.addWidget(res2, alignment=(Qt.AlignCenter), stretch=2)
+StatisticsGroupBox.setLayout(layout_line4)
 layout_line1 = QHBoxLayout()
 layout_line2 = QHBoxLayout()
 layout_line3 = QHBoxLayout()
@@ -49,7 +57,9 @@ layout_line3 = QHBoxLayout()
 layout_line1.addWidget(lb_q, alignment=(Qt.AlignCenter))
 layout_line2.addWidget(RadioGroupBox)
 layout_line2.addWidget(ResultGroupBox)
+layout_line2.addWidget(StatisticsGroupBox)
 ResultGroupBox.hide()
+StatisticsGroupBox.hide()
 layout_line3.addStretch(1)
 layout_line3.addWidget(btn_OK, stretch=3)
 layout_line3.addStretch(1)
@@ -61,20 +71,27 @@ layout_card.addStretch(1)
 layout_card.addLayout(layout_line3, stretch=1)
 layout_card.addStretch(1)
 ia = 0
-
+grade = 0
 def true_variant():
+    global grade
     if lb_q.text() == "Сколько будет 1000-7":
         variant = [rbtn_1, rbtn_2, rbtn_3, rbtn_4]
         if variant[3].isChecked():
-            res.setText("Правильно")
+            tf.setText("Правильно")
+            res.setText(variant[3].text())
+            grade += 50
         else:
-            res.setText(f"Неправильно, правильный ответ: {variant[3].text()}")
+            tf.setText("Неправильно")
+            res.setText(f"Правильный ответ: {variant[3].text()}")
     else:
         variant = [rbtn_1, rbtn_2, rbtn_3, rbtn_4]
         if variant[2].isChecked():
-            res.setText("Правильно")
+            tf.setText("Правильно")
+            res.setText(variant[2].text())
+            grade += 50
         else:
-            res.setText(f"Неправильно, правильный ответ: {variant[2].text()}")
+            tf.setText("Неправильно")
+            res.setText(f"Правильный ответ: {variant[2].text()}")
 
 def ok():
     true_variant()
@@ -98,6 +115,16 @@ def show_question():
     rbtn_3.setText("56")
     rbtn_4.setText("12")
 
+def statistics():
+    global grade
+    RadioGroupBox.hide()
+    ResultGroupBox.hide()
+    StatisticsGroupBox.show()
+    lb_q.setText("-Опрос Окончен-")
+    btn_OK.setText("Закрыть Программу")
+    res2.setText(f"Ваш бал {str(grade)}/100")
+
+
 def test():
     global ia
     if 'Ответить' == btn_OK.text():
@@ -106,10 +133,11 @@ def test():
         show_question()
 
     if ia == 3:
-        sys.exit()
-        
+        statistics() 
     elif ia == 2:
-        btn_OK.setText("Закрыть тест")
+        btn_OK.setText("Стастистика")
+    elif ia == 4:
+        exit()
     ia += 1
 
 btn_OK.clicked.connect(test)
